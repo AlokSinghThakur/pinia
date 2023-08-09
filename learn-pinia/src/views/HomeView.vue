3edd6105acc395d38b133d3470e941d6c55f87e8
Alok Singh Thakur <aloksinghthakurcse@gmail.com>
2023-08-09 16:15:53 +0530

    pinia intialize

---
+++ learn-pinia/src/views/HomeView.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="home">
+    <div class="count" >{{ storeCounter.count }}</div>
+    <div class="button">
+      <button @click=increaseCount>+</button>
+      <button @click=decreaseCount>-</button>
+    </div>
+    <hr>
+    <div>
+      This number is : {{ oddOrEven }}
+    </div>
+  </div>
+</template>
+
+<script setup>
+import {ref,computed} from 'vue'
+import { useCounterStore } from '@/stores/counter';
+
+const storeCounter = useCounterStore()
+const count = ref(10)
+
+const increaseCount = () => {
+  count.value++
+}
+
+
+const decreaseCount = () => {
+  count.value--
+}
+
+const oddOrEven = computed(() => {
+  if(count.value%2==0){
+    return 'even'
+  }
+  return 'odd'
+})
+</script>
+<style>
+.count {
+  font-size: 60px;
+  margin: 20px;
+}
+.buttons button {
+  font-size: 40px;
+  margin: 10px;
+}
+</style>
